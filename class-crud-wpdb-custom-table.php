31f1f10d9cff8e9f926577375fb9beaeddfd55a8
Hoang Bui <hoangict@hotmail.com>
2022-03-23 13:51:40 +0700

    Create custom table

--- class-crud-wpdb-custom-table.php
+++ class-crud-wpdb-custom-table.php
@@ -75,3 +75,26 @@ class CRUDCustomTable {
     }
 
 }
+
+/*
+ * Create custom table
+ */
+register_activation_hook(__FILE__, 'crud_create_custom_table');
+
+function crud_create_custom_table() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'table_name';
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+            ID mediumint(9) NOT NULL AUTO_INCREMENT,
+            created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            PRIMARY KEY  (ID)
+	) $charset_collate;";
+
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+}
